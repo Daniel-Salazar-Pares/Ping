ca66ea4ac7628c6635e2d10c6f7c41fea770f178
Daniel Salazar <daniel.salazar.7e7@itb.cat>
2024-04-16 09:39:36 +0200

    end

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,11 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 fun main() {
+    var pb = ProcessBuilder("notepad.exe”,”1.txt")
+    val p:Process = pb.start()
     repeat (1000) {
         Thread.sleep(1000)
         val current = LocalDateTime.now()
@@ -9,7 +13,13 @@ fun main() {
         val formatted = current.format(formatter)
         val ping = "$formatted - Hola"
         println(ping)
+        p.outputStream.write(ping.toByteArray())
+        p.outputStream.flush()
 
-
+        val reader = BufferedReader(InputStreamReader(p.inputStream))
+        val pong = reader.readLine()
+        if (pong != null) {
+            println(pong)
+        }
     }
 }
