50623f2520199ae006be6c6914d3a6642fa9e986
Daniel Salazar <daniel.salazar.7e7@itb.cat>
2024-04-17 13:05:06 +0200

    end

--- src/Main.kt
+++ src/Main.kt
@@ -1,25 +1,28 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.lang.Thread.sleep
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.util.Scanner
 
 fun main() {
-    var pb = ProcessBuilder("notepad.exe”,”1.txt")
-    val p:Process = pb.start()
-    val formatter = DateTimeFormatter.ofPattern("HH:mm:ss")
-    repeat (1000) {
-        Thread.sleep(1000)
+        var pb = ProcessBuilder("java", "-jar", "C:\\Users\\user\\IdeaProjects\\pong\\out\\artifacts\\pong_jar\\pong.jar")
+        val p: Process = pb.start()
+
+
+        //definim el format de l0hora i la guardem a una variable
+        val formatter = DateTimeFormatter.ofPattern("HH:mm:ss")
+
         val current = LocalDateTime.now()
         val formatted = current.format(formatter)
-        val ping = "$formatted - Hola"
-        println(ping)
-        p.outputStream.write(ping.toByteArray())
-        p.outputStream.flush()
+        val ping = "$formatted - Hola\n"
+        print(ping)
 
+        //llegim la resposta del pong
+        p.outputStream.write(ping.toByteArray())
         val reader = BufferedReader(InputStreamReader(p.inputStream))
-        val pong = reader.readLine()
-        if (pong != null) {
-            println(pong)
-        }
-    }
+        var pong = reader.readLine()
+        println(pong)
+
+
 }
